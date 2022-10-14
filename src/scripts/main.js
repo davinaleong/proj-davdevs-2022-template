6597ae9dd9da8b7ca9bacf838815fd5d237a72e4
Davina Leong <davina.leong.sy@gmail.com>
2022-10-14 14:47:11 +0800

    Able to toggle menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 console.log(`main.js loaded`)
+
+const mainHeaderDropdownToggleEls = document.querySelectorAll(
+  `[data-element="main-header-dropdown-toggle"]`
+)
+const mainHeaderDropdownMenuEls = document.querySelectorAll(
+  `[data-element="main-header-dropdown-menu"]`
+)
+
+mainHeaderDropdownToggleEls.forEach((mainHeaderDropdownToggleEl) => {
+  mainHeaderDropdownToggleEl.addEventListener(`click`, (e) => {
+    const tagName = e.target.tagName
+    const parentEl =
+      tagName === `DIV`
+        ? e.target.parentElement.parentElement
+        : e.target.parentElement
+
+    if (parentEl.childNodes.length >= 4) {
+      mainHeaderDropdownMenuEls.forEach((mainHeaderDropdownMenuEls) =>
+        mainHeaderDropdownMenuEls.removeAttribute(`data-expanded`)
+      )
+
+      const dropdownEl = parentEl.childNodes[3]
+      dropdownEl.setAttribute(`data-expanded`, true)
+    }
+  })
+})
