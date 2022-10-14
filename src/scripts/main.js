88992896407876b8cf1b26abf2a35aa022d11f66
Davina Leong <davina.leong.sy@gmail.com>
2022-10-14 15:07:18 +0800

    Able to toggle mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
 console.log(`main.js loaded`)
 
+const attrDataExpaned = `data-expanded`
+
+const mainHeaderEl = document.querySelector(`[data-element="main-header"]`)
+const mainHeaderExpandEl = document.querySelector(`[data-element="main-header-expand"]`)
+const mainHeaderCloseEl = document.querySelector(`[data-element="main-header-close"]`)
+
 const mainHeaderDropdownToggleEls = document.querySelectorAll(
   `[data-element="main-header-dropdown-toggle"]`
 )
 const mainHeaderDropdownMenuEls = document.querySelectorAll(
   `[data-element="main-header-dropdown-menu"]`
 )
+
+mainHeaderExpandEl.addEventListener(`click`, (e) => {
+    mainHeaderEl.setAttribute(attrDataExpaned, true)
+})
+
+mainHeaderCloseEl.addEventListener('click', (e) => {
+    mainHeaderEl.removeAttribute(attrDataExpaned)
+})
 
 mainHeaderDropdownToggleEls.forEach((mainHeaderDropdownToggleEl) => {
   mainHeaderDropdownToggleEl.addEventListener(`click`, (e) => {
@@ -17,11 +31,11 @@ mainHeaderDropdownToggleEls.forEach((mainHeaderDropdownToggleEl) => {
 
     if (parentEl.childNodes.length >= 4) {
       mainHeaderDropdownMenuEls.forEach((mainHeaderDropdownMenuEls) =>
-        mainHeaderDropdownMenuEls.removeAttribute(`data-expanded`)
+        mainHeaderDropdownMenuEls.removeAttribute(attrDataExpaned)
       )
 
       const dropdownEl = parentEl.childNodes[3]
-      dropdownEl.setAttribute(`data-expanded`, true)
+      dropdownEl.setAttribute(attrDataExpaned, true)
     }
   })
 })
