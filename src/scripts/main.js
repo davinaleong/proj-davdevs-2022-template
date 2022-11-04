b79793d4f43777f8ba832873f385c080dd22c392
Davina Leong <leong.shi.yun@gmail.com>
2022-11-04 12:47:33 +0800

    Able to remove alert

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ console.log(`main.js loaded`)
 
 const attrDataExpaned = `data-expanded`
 
+// Main Header
 const mainHeaderEl = document.querySelector(`[data-element="main-header"]`)
 const mainHeaderExpandEl = document.querySelector(
   `[data-element="main-header-expand"]`
@@ -9,7 +10,6 @@ const mainHeaderExpandEl = document.querySelector(
 const mainHeaderCloseEl = document.querySelector(
   `[data-element="main-header-close"]`
 )
-
 const mainHeaderDropdownToggleEls = document.querySelectorAll(
   `[data-element="main-header-dropdown-toggle"]`
 )
@@ -46,4 +46,14 @@ mainHeaderDropdownToggleEls.forEach((mainHeaderDropdownToggleEl) => {
       dropdownEl.setAttribute(attrDataExpaned, true)
     }
   })
+})
+
+// Header Alert
+const alertRemovedAttr = `data-alert-removed`
+const resumeBody = document.querySelector(`[data-element="body-resume"]`)
+const alertEl = document.querySelector(`[data-element="alert"]`)
+const alertCloseEl = document.querySelector(`[data-element="alert-close"]`)
+alertCloseEl.addEventListener(`click`, (e) => {
+  resumeBody.setAttribute(alertRemovedAttr, true)
+  alertEl.remove()
 })
